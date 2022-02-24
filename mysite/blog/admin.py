a821b4b4400a3e3068378549fc1d629bd58b89bb
Saeid Gholami <saeidxgholami@gmail.com>
2022-02-24 22:11:35 +0330

    ch 01:

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "author", "publish", "status")
+    list_filter = ("status", "created", "publish", "author")  # side bar
+    search_fields = ("title", "body")  # create search bar
+    prepopulated_fields = {"slug": ("title",)}  # auto fill slug with title
+    raw_id_fields = ("author",)  # lookup field for author rather than dropdown
+    date_hierarchy = "publish"  # links to navigate through a date hierarchy
+    ordering = ("status", "publish")
